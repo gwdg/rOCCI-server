1a7e69152f4c8802bc73dffb4a364bf6628c3f80
Boris Parak <araxxxx@gmail.com>
2012-12-04 20:13:09 +0100

    Added support for VOMS proxy cert. attributes
    * Basic implementation, should be replaced later
      with modified mod_gridsite
    * Requires +ExportCertData in Apache2 VH SSLOptions
      and "export OPENSSL_ALLOW_PROXY_CERTS=1" in ENVVARS
    * Found VOMS attributes are appended to user's DN
      e.g. /DC=org/DC=organization/CN=Name/VO=vo.name.org/Role=NULL/Capability=NULL

--- lib/occi/frontend/http/http_frontend.rb
+++ lib/occi/frontend/http/http_frontend.rb
@@ -31,42 +31,20 @@ module OCCI
             server.halt 403, "Password in request does not match password of user #{username}" unless @backend.authorized?(username, password)
             username
           elsif request.env['SSL_CLIENT_S_DN']
-            # For https, the web service should be set to include the user cert in the environment.
-
+            # For https, the web service should be set to include the user cert in the environment
             cert_subject = request.env['SSL_CLIENT_S_DN']
-            # Password should be DN with whitespace removed.
-            OCCI::Log.debug "Looking for: #{cert_subject}"
+
+            # Password should be DN with whitespaces removed
+            OCCI::Log.debug "Looking for user #{cert_subject}"
             username     = @backend.get_username(cert_subject)
 
+            # Lookup failed with SSL_CLIENT_S_DN, we should try handling the certificate as a VOMS proxy
             if username.nil?
               OCCI::Log.debug "User not found! Attempting to handle proxy certificates..."
+              cert_subject = check_voms_proxy_cert
 
-              OCCI::Log.debug "Looking for SSL_CLIENT_S_DN_CN_* env variables..."
-              last_cn = 0
-              (1..10).each do |i|
-                if request.env["SSL_CLIENT_S_DN_CN_#{i}"]
-                  last_cn = i
-                else
-                  break
-                end
-              end
-
-              if last_cn > 0
-                cert_issuer = request.env['SSL_CLIENT_I_DN']
-                proxy_cn = request.env["SSL_CLIENT_S_DN_CN_#{last_cn}"]
-                proxy_subject = request.env['SSL_CLIENT_S_DN'].gsub("/CN=#{proxy_cn}", '')
-
-                OCCI::Log.debug "#{proxy_subject} == #{cert_issuer}?"
-
-                if proxy_subject == cert_issuer
-                  username = @backend.get_username(cert_issuer)
-                else
-                  OCCI::Log.debug "Issuer DN doesn't match stripped cert DN, I won't accept this proxy!"
-                  username = nil
-                end
-              else
-                OCCI::Log.debug "This is not a RFC compliant proxy certificate, there is nothing I can do!"
-              end
+              OCCI::Log.debug "Looking for user #{cert_subject}"
+              username     = @backend.get_username(cert_subject)
             end
 
             OCCI::Log.debug "Username: #{username ? username : 'nil' }"
@@ -77,6 +55,63 @@ module OCCI
             'anonymous'
           end
         end
+
+        def check_voms_proxy_cert
+          proxy_cert_subject = nil
+
+          # Proxy certs append CNs, we have to find the last one
+          last_cn = 0
+          (1..256).each do |i|
+            if request.env["SSL_CLIENT_S_DN_CN_#{i}"]
+              last_cn = i
+            else
+              break
+            end
+          end
+
+          # Proxy cert has to have at least SSL_CLIENT_S_DN_CN_1
+          if last_cn > 0
+            # Just to be sure, we should get issuer's DN
+            # and compare it to subject's DN
+            cert_issuer = request.env['SSL_CLIENT_I_DN']
+            proxy_cn = request.env["SSL_CLIENT_S_DN_CN_#{last_cn}"]
+            proxy_cert_subject = request.env['SSL_CLIENT_S_DN'].gsub("/CN=#{proxy_cn}", '')
+
+            OCCI::Log.debug "Proxy DN: #{proxy_cert_subject}?"
+            if proxy_cert_subject == cert_issuer
+              # SSL_CLIENT_CERT should contain client's certificate
+              if request.env['SSL_CLIENT_CERT']
+                # Read client's certificate
+                proxy_cert = OpenSSL::X509::Certificate.new request.env['SSL_CLIENT_CERT']
+
+                # Iterate through available extensions
+                voms_ary = nil
+                proxy_cert.extensions.each do |ext|
+                  # Find VOMS extensions using their OID
+                  if ext.oid == "1.3.6.1.4.1.8005.100.100.5"
+                    # Parse group and role from cert extension
+                    # TODO: use ASN1 or mod_gridsite
+                    voms_ary = /\*\/(.+)\/Role=(.+)\/Capability=NULL/.match ext.value
+                    OCCI::Log.debug "VOMS ext: group=#{voms_ary[1]} role=#{voms_ary[2]}"
+                  end
+                end
+
+                # Append found values to user's DN
+                if voms_ary && voms_ary[1] && voms_ary[2]
+                  proxy_cert_subject = proxy_cert_subject << "/VO=#{voms_ary[1]}/Role=#{voms_ary[2]}/Capability=NULL"
+                end
+              else
+                OCCI::Log.warn "SSL_CLIENT_CERT is not available, add +ExportCertData to SSLOptions!"
+              end
+            else
+              OCCI::Log.warn "Issuer DN doesn't match stripped cert DN, I won't accept this proxy!"
+              proxy_cert_subject = nil
+            end
+          end
+
+          proxy_cert_subject
+        end
+
       end
     end
   end
