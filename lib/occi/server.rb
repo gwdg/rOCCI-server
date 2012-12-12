cc5b12aae5c39bef168f964b52fb1d1c3f748c21
Boris Parak <araxxxx@gmail.com>
2012-12-12 13:24:10 +0100

    Minor version bump to 0.5.2

--- lib/occi/server.rb
+++ lib/occi/server.rb
@@ -18,7 +18,7 @@ module OCCI
     set :views, File.dirname(__FILE__) + "/../../views"
     enable :logging
 
-    VERSION = "0.5.0"
+    VERSION = "0.5.2"
 
     register Sinatra::MultiRoute
     register Sinatra::CrossOrigin
