abde4d36efb4fc25a2f2d9c4ffcf101e080e882d
Boris Parak <parak@cesnet.cz>
2013-10-14 22:00:08 +0200

    Added dynamic strategy loading for Warden
    * Strategies are now configurable in 'config/config.yml'
    * Discovery and loading of strategies at runtime from
      'lib/authentication_strategies'

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,9 +1,23 @@
+# Insert Warden::Manager as Rack::Middleware
 Rails.configuration.middleware.insert_before Rack::Head, Warden::Manager do |manager|
   manager.default_strategies :dummy
   manager.failure_app = UnauthorizedController
   manager.scope_defaults :default, :store => false
 end
 
-# TODO: implement autoload from /app/strategies
-ActiveSupport::Dependencies.autoload_paths << "#{Rails.root}/lib"
-Warden::Strategies.add(:dummy, AuthenticationStrategies::DummyStrategy)
+# Autoload strategies from Rails.root/lib/authentication_strategies
+ActiveSupport::Dependencies.autoload_paths << Rails.root.join('lib')
+
+# Enable strategies selected in Rails.root/config/config.yml
+ROCCI_SERVER_CONFIG['authn'].each do |authn_strategy|
+  authn_strategy = "#{authn_strategy.classify}Strategy"
+  Rails.logger.info "AuthN subsystem: Enabling #{authn_strategy}"
+
+  begin
+    Warden::Strategies.add(:dummy, AuthenticationStrategies.const_get("#{authn_strategy}"))
+  rescue NameError => err
+    message = "There is no such authentication strategy available! [AuthenticationStrategies::#{authn_strategy}]"
+    Rails.logger.error message
+    raise ArgumentError, message
+  end
+end
