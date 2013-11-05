69b31c2131b0a616ae516ec1eb5557d42f302f0f
Boris Parak <parak@cesnet.cz>
2013-11-05 18:46:51 +0100

    Added basic specs for OcciModel

--- spec/models/occi_model_spec.rb
+++ spec/models/occi_model_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe OcciModel do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'self.get' do
+    it 'returns Occi::Model' do
+      expect(OcciModel.get).to be_kind_of(Occi::Model)
+    end
+
+    it 'return non-empty Occi::Model' do
+      expect(OcciModel.get).not_to be_empty
+    end
+  end
+
+  context 'self.get_filtered' do
+    it 'returns Occi::Model' do
+      expect(OcciModel.get_filtered(Occi::Collection.new)).to be_kind_of(Occi::Model)
+    end
+
+    it 'return non-empty Occi::Model' do
+      expect(OcciModel.get_filtered(nil)).not_to be_empty
+    end
+  end
+
+  context 'self.get_extensions' do
+    it 'returns Occi::Collection' do
+      expect(OcciModel.send(:get_extensions)).to be_kind_of(Occi::Collection)
+    end
+  end
+
 end
