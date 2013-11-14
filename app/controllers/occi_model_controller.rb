63ed71f5a58cc141736ea2a15dd264c96f0e4e07
Boris Parak <parak@cesnet.cz>
2013-11-14 22:21:22 +0100

    Added stubs with 501 to OcciModelController

--- app/controllers/occi_model_controller.rb
+++ app/controllers/occi_model_controller.rb
@@ -1,5 +1,6 @@
 class OcciModelController < ApplicationController
 
+  # GET /
   def index
     resources = Occi::Core::Resources.new
 
@@ -10,15 +11,27 @@ class OcciModelController < ApplicationController
     respond_with(resources)
   end
 
+  # GET /-/
+  # GET /.well-known/org/ogf/occi/-/
   def show
     model = OcciModel.get(request_occi_collection)
     respond_with(model)
   end
 
+  # POST /-/
+  # POST /.well-known/org/ogf/occi/-/
   def create
+    # TODO: impl
+    collection = Occi::Collection.new
+    respond_with(collection, status: 501)
   end
 
+  # DELETE /-/
+  # DELETE /.well-known/org/ogf/occi/-/
   def delete
+    # TODO: impl
+    collection = Occi::Collection.new
+    respond_with(collection, status: 501)
   end
 
 end
