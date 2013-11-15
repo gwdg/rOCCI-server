c4bf43e05d6aa9b395b30718c88d9ae988e0dd81
Boris Parak <parak@cesnet.cz>
2013-11-15 22:41:17 +0100

    Updated custom renderers
    Added support for link_only rendering in all content types.

--- config/initializers/occi_renderers.rb
+++ config/initializers/occi_renderers.rb
@@ -1,27 +1,47 @@
 # Render OCCI into HTTP headers for 'text/occi'
 ActionController::Renderers.add :occi_header do |obj, options|
   self.content_type ||= Mime::OCCI_HEADER
-  self.headers.merge!(obj.to_header) if obj.respond_to?(:to_header)
 
-  ""
+  if options[:flag] == :link_only
+    self.headers['Location'] = obj
+    "OK"
+  else
+    self.headers.merge!(obj.to_header) if obj.respond_to?(:to_header)
+    ""
+  end
 end
 
 # Render OCCI into HTTP body for 'text/plain'
 ActionController::Renderers.add :text do |obj, options|
   self.content_type ||= Mime::TEXT
-  obj.respond_to?(:to_text) ? obj.to_text : obj
+
+  if options[:flag] == :link_only
+    "X-OCCI-Location: #{obj}"
+  else
+    obj.respond_to?(:to_text) ? obj.to_text : obj
+  end
 end
 
 # Render OCCI into HTTP body for 'application/occi+json'
 ActionController::Renderers.add :occi_json do |obj, options|
   self.content_type ||= Mime::OCCI_JSON
-  obj.respond_to?(:to_json) ? obj.to_json : "{ 'message': 'Object cannot be rendered as JSON!' }"
+
+  if options[:flag] == :link_only
+    "{ 'Location': '#{obj}' }"
+  else
+    obj.respond_to?(:to_json) ? obj.to_json : "{ 'message': 'Object cannot be rendered as JSON!' }"
+  end
 end
 
 # Render OCCI into HTTP body for 'application/occi+xml'
 ActionController::Renderers.add :occi_xml do |obj, options|
   self.content_type ||= Mime::OCCI_XML
-  obj.respond_to?(:to_xml) ? obj.to_xml : "<message>Object cannot be rendered as XML!</message>"
+
+  if options[:flag] == :link_only
+    "<location>#{obj}</location>"
+  else
+    obj.respond_to?(:to_xml) ? obj.to_xml : "<message>Object cannot be rendered as XML!</message>"
+  end
 end
 
 # Render OCCI locations into HTTP body for 'text/uri-list'
