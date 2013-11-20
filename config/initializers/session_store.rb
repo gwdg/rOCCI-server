896f926c2c48ea97a227ee0b8c328023ee677626
Boris Parak <parak@cesnet.cz>
2013-11-20 23:22:28 +0100

    Switched to dalli(== memcache) for caching
    * Later will provide namespaced caches for backends

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
-
-ROCCIServer::Application.config.session_store :cookie_store, key: '_rOCCI-server_session'
+if Rails.env.production?
+  require 'action_dispatch/middleware/session/dalli_store'
+  ROCCIServer::Application.config.session_store :dalli_store, :memcache_server => ['localhost'], :namespace => 'sessions', :key => '_foundation_session', :expire_after => 20.minutes
+else
+  ROCCIServer::Application.config.session_store :cookie_store, key: '_rOCCI-server_session'
+end
