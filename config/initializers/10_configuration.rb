4a849656ce564d17beab011633ed8f65d572b656
Boris Parak <parak@cesnet.cz>
2013-11-27 21:31:08 +0100

    Refactored config files and new config loader

--- config/initializers/10_configuration.rb
+++ config/initializers/10_configuration.rb
@@ -4,7 +4,10 @@ ROCCI_SERVER_CONFIG = Hashie::Mash.new
 def get_yaml_config(path, env = Rails.env)
   begin
     Rails.logger.info "[Configuration] Loading configuration from #{path} for ENV #{env}."
-    YAML.load(ERB.new(File.read(path)).result)[env]
+    raise "Does not exist!" unless Dir.exists?(path)
+
+    path = path.join("#{env}.yml")
+    YAML.load(ERB.new(File.read(path)).result) || {}
   rescue Exception => err
     message = "Failed to parse a configuration file! [#{path}]: #{err.message}"
     Rails.logger.error "[Configuration] #{message}"
@@ -13,14 +16,37 @@ def get_yaml_config(path, env = Rails.env)
 end
 private :get_yaml_config
 
-# Load general configuration from 'etc/common.yml'
-ROCCI_SERVER_CONFIG.common = get_yaml_config(Rails.root.join('etc', 'common.yml')).deep_freeze
+# Load general configuration from 'etc/*.yml'
+ROCCI_SERVER_CONFIG.common = get_yaml_config(Rails.root.join('etc'))
 
-# Load hook configuration from 'etc/hooks.yml'
-ROCCI_SERVER_CONFIG.hooks = get_yaml_config(Rails.root.join('etc', 'hooks.yml')).deep_freeze
+# Load hook configuration from 'etc/hooks/**/*.yml'
+unless ROCCI_SERVER_CONFIG.common.hooks.respond_to?(:each)
+  if ROCCI_SERVER_CONFIG.common.hooks
+    ROCCI_SERVER_CONFIG.common.hooks = [ROCCI_SERVER_CONFIG.common.hooks]
+  else
+    ROCCI_SERVER_CONFIG.common.hooks = []
+  end
+end
 
-# Load backend configuration from 'etc/backends.yml'
-ROCCI_SERVER_CONFIG.backends = get_yaml_config(Rails.root.join('etc', 'backends.yml')).deep_freeze
+ROCCI_SERVER_CONFIG.common.hooks.each do |hook|
+  ROCCI_SERVER_CONFIG.hooks![hook] = get_yaml_config(Rails.root.join('etc', 'hooks', hook))
+end
 
-# Load backend configuration from 'etc/authn_strategies.yml'
-ROCCI_SERVER_CONFIG.authn_strategies = get_yaml_config(Rails.root.join('etc', 'authn_strategies.yml')).deep_freeze
+# Load backend configuration from 'etc/backends/**/*.yml'
+ROCCI_SERVER_CONFIG.backends![ROCCI_SERVER_CONFIG.common.backend] = get_yaml_config(Rails.root.join('etc', 'backends', ROCCI_SERVER_CONFIG.common.backend))
+
+# Load backend configuration from 'etc/authn_strategies/**/*.yml'
+unless ROCCI_SERVER_CONFIG.common.authn_strategies.respond_to?(:each)
+  if ROCCI_SERVER_CONFIG.common.authn_strategies
+    ROCCI_SERVER_CONFIG.common.authn_strategies = [ROCCI_SERVER_CONFIG.common.authn_strategies]
+  else
+    ROCCI_SERVER_CONFIG.common.authn_strategies = []
+  end
+end
+
+ROCCI_SERVER_CONFIG.common.authn_strategies.each do |authn_strategy|
+  ROCCI_SERVER_CONFIG.authn_strategies![authn_strategy] = get_yaml_config(Rails.root.join('etc', 'authn_strategies', authn_strategy))
+end
+
+# Freeze the config
+ROCCI_SERVER_CONFIG.deep_freeze
