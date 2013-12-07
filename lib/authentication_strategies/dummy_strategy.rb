c1da6671791fcdb61fb67fb793ca7301105bbde1
Boris Parak <parak@cesnet.cz>
2013-12-07 14:29:05 +0100

    Updated DummyStrategy
    * Dynamic configuration
    * Ability to fake other strategies

--- lib/authentication_strategies/dummy_strategy.rb
+++ lib/authentication_strategies/dummy_strategy.rb
@@ -19,9 +19,21 @@ module AuthenticationStrategies
       end
 
       user = Hashie::Mash.new
-      user.auth!.type = "dummy"
-      user.auth!.credentials!.username = OPTIONS.fake_username || "dummy_user"
-      user.auth!.credentials!.password = OPTIONS.fake_password || "dummy_password"
+      user.auth!.type = OPTIONS.fake_type || "dummy"
+
+      case user.auth.type
+      when "dummy", "basic"
+        user.auth!.credentials!.username = OPTIONS.fake_username || "dummy_user"
+        user.auth!.credentials!.password = OPTIONS.fake_password || "dummy_password"
+      when "x509", "voms"
+        user.auth!.credentials!.client_cert_dn = OPTIONS.fake_client_cert_dn || "dummy_cert_dn"
+        user.auth!.credentials!.client_cert = OPTIONS.fake_client_cert || "dummy_cert"
+        user.auth!.credentials!.client_cert_voms_attrs = OPTIONS.fake_voms_attrs || {}
+        user.auth!.credentials!.issuer_cert_dn = OPTIONS.fake_issuer_cert_dn || "dummy_issuer_cert_dn"
+        user.auth!.credentials!.verification_status = OPTIONS.fake_verification_status || "SUCCESS"
+      else
+        user.auth!.credentials = {}
+      end
 
       Rails.logger.debug "[AuthN] [#{self.class}] Authenticated #{user.to_hash.inspect}"
       success! user
