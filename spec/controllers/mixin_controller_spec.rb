6a255b9119edd240d999084cecedeb78f7268743
Boris Parak <parak@cesnet.cz>
2014-01-20 20:47:30 +0100

    Fixed minor stuff in specs
    Routing specs are still broken (?!)

--- spec/controllers/mixin_controller_spec.rb
+++ spec/controllers/mixin_controller_spec.rb
@@ -4,11 +4,4 @@ describe MixinController do
 
   pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "GET 'index'" do
-   it "returns http success" do
-     get 'index', { term: 'my_stuff/test' }
-     expect(response).to be_success
-   end
-  end
-
 end
--- spec/controllers/storagelink_controller_spec.rb
+++ spec/controllers/storagelink_controller_spec.rb
@@ -5,9 +5,9 @@ describe StoragelinkController do
   pending "add some examples to (or delete) #{__FILE__}"
 
   describe "GET 'show'" do
-   it "returns http success" do
+   it "returns not found" do
      get 'show', { id: 'df45ad6f4adf-daf4d5f6a4d-adf54ad5f6ad' }
-     expect(response).to be_success
+     expect(response.status).to eq(404)
    end
   end
 
