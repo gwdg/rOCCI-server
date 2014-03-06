853861949de677396460e81755dfe20dfc486ea4
Boris Parak <parak@cesnet.cz>
2014-03-06 23:56:12 +0100

    Fixed YamlHelper (static context)

--- lib/authentication_strategies/helpers/yaml_helper.rb
+++ lib/authentication_strategies/helpers/yaml_helper.rb
@@ -2,12 +2,12 @@ module AuthenticationStrategies::Helpers
   # Set of helpers for working with YAML
   module YamlHelper
     # Helps with reading YAML files with ERB snippets
-    def read_yaml(path)
+    def self.read_yaml(path)
       begin
         raise "File does not exist!" unless File.exists?(path)
         YAML.load(ERB.new(File.read(path)).result)
       rescue Exception => err
-        raise Errors::ConfigurationParsingError,
+        raise ::Errors::ConfigurationParsingError,
               "Failed to parse a YAML file! [#{path}]: #{err.message}"
       end
     end
