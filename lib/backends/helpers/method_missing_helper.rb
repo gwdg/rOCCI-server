354cccd427243d0164287822c4c0a65c065dac7f
Boris Parak <parak@cesnet.cz>
2014-03-09 11:45:46 +0100

    Fixed method_missing for backends
    `method_missing` should not be implemented as a catch-all solution.
    Instead, this version catches only relevant missing public API methods
    and delegates the rest to the original implementation.

--- lib/backends/helpers/method_missing_helper.rb
+++ lib/backends/helpers/method_missing_helper.rb
@@ -2,7 +2,11 @@ module Backends
   module Helpers
     module MethodMissingHelper
       def method_missing(m, *args, &block)
-        fail Backends::Errors::MethodNotImplementedError, "Method is not implemented in the #{self.class.to_s} backend! [#{m}]"
+        if m.to_s.match(/compute_|network_|os_tpl_|resource_tpl_|storage_/)
+          fail Backends::Errors::MethodNotImplementedError, "Method is not implemented in the #{self.class.to_s} backend! [#{m}]"
+        else
+          super # This has to be here! Look at the previous line in the backtrace to find out what really happened!
+        end
       end
     end
   end
