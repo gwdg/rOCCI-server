03b287eb669d4c866c4e8fcd9fa0c21b20da5a1d
Boris Parak <parak@cesnet.cz>
2014-03-09 15:11:03 +0100

    Fixed init of Occi::Log

--- config/initializers/20_occi_logger.rb
+++ config/initializers/20_occi_logger.rb
@@ -1,3 +1,8 @@
-# Initialize ::Occi::Log and change log_level if applicable
-Rails.application.config.log_level = ::Occi::Log.const_get(ROCCI_SERVER_CONFIG.common.log_level.upcase.to_sym) if ROCCI_SERVER_CONFIG.common.log_level
-_logger = ::Occi::Log.new(Rails.application.config.logger)
+# Initialize ::Occi::Log
+logger = ::Occi::Log.new(Rails.logger)
+
+# Change logging level if applicable
+unless ROCCI_SERVER_CONFIG.common.log_level.blank?
+  logger.level = ::Occi::Log.const_get(ROCCI_SERVER_CONFIG.common.log_level.upcase.to_sym)
+  Rails.application.config.log_level = Rails.logger.class.const_get(ROCCI_SERVER_CONFIG.common.log_level.upcase.to_sym)
+end
