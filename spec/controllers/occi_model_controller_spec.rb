a6c4c076a41c2e34df45d1d74e2beac913406c4e
Boris Parak <parak@cesnet.cz>
2014-04-02 09:00:29 +0200

    Specs for MixinController and OcciModelController
    Added basic specs for another batch of controllers, still WIP

--- spec/controllers/occi_model_controller_spec.rb
+++ spec/controllers/occi_model_controller_spec.rb
@@ -2,15 +2,69 @@ require 'spec_helper'
 
 describe OcciModelController do
 
-  pending "add some examples to (or delete) #{__FILE__}"
-
   describe "GET 'index'" do
-    it 'returns http success' do
-      @request.env['HTTP_ACCEPT'] = 'application/json'
-      @request.env['CONTENT_TYPE'] = 'application/json'
-      get 'index'
+
+    it 'returns http success with JSON' do
+      get 'index', format: :occi_json
       expect(response).to be_success
     end
+
+    it 'returns http success with URI-LIST' do
+      get 'index', format: :uri_list
+      expect(response).to be_success
+    end
+
+    it 'returns http not acceptable with PLAIN' do
+      expect { get 'index', format: :text }.to raise_error
+    end
+
+    it 'returns http not acceptable with OCCI' do
+      expect { get 'index', format: :occi_header }.to raise_error
+    end
+
+  end
+
+  describe "GET 'show'" do
+
+    let(:fake_app) { Proc.new {} }
+    before(:each){
+      @request.env['rocci_server.request.parser'] ||= ::RequestParsers::OcciParser.new(fake_app)
+      @request.env['rocci_server.request.parser'].call(@request.env)
+    }
+
+    it 'returns http success with JSON' do
+      get 'show', format: :occi_json
+      expect(response).to be_success
+    end
+
+    it 'returns http success with PLAIN' do
+      get 'show', format: :text
+      expect(response).to be_success
+    end
+
+    it 'returns http success with OCCI' do
+      get 'show', format: :occi_header
+      expect(response).to be_success
+    end
+
+  end
+
+  describe "POST 'create'" do
+
+    it 'returns http not implemented' do
+      post 'create', format: :text
+      expect(response.status).to eq 501
+    end
+
+  end
+
+  describe "DELETE 'delete'" do
+
+    it 'returns http not implemented' do
+      delete 'delete', format: :text
+      expect(response.status).to eq 501
+    end
+
   end
 
 end
