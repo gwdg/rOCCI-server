e9b6f352369ae34a0a97a643c266c0876637c4c3
Boris Parak <parak@cesnet.cz>
2014-04-03 18:55:09 +0200

    Simplified DummyBackend using metaprogramming
    Generating groups of similar fixture-handling methods on the fly with
    metaprogramming and `class_eval`.

--- lib/backends/dummy_backend.rb
+++ lib/backends/dummy_backend.rb
@@ -1,7 +1,8 @@
 module Backends
   class DummyBackend
     API_VERSION = '0.0.1'
-    FIXTURES = [:compute, :network, :storage, :os_tpl, :resource_tpl].freeze
+    FIXTURES = [:compute, :network, :storage].freeze
+    FIXTURES_TPL = [:os_tpl, :resource_tpl].freeze
 
     def initialize(delegated_user, options, server_properties, logger, dalli_cache)
       @delegated_user = Hashie::Mash.new(delegated_user)
@@ -16,116 +17,62 @@ module Backends
 
     def read_fixtures(base_path)
       @logger.debug "[Backends] [DummyBackend] Reading fixtures from #{base_path.to_s.inspect}"
-      FIXTURES.each { |name| send "read_#{name.to_s}_fixtures", base_path }
+      (FIXTURES + FIXTURES_TPL).each { |name| send "read_#{name.to_s}_fixtures", base_path }
     end
 
-    def read_compute_fixtures(path = '')
-      compute = Rails.env.test? ? @compute : @dalli_cache.get('dummy_compute')
+    FIXTURES.each do |fixture|
+      class_eval %Q|
+def read_#{fixture}_fixtures(path = '')
+  #{fixture} = Rails.env.test? ? @#{fixture} : @dalli_cache.get('dummy_#{fixture}')
 
-      unless compute
-        path = path_for_fixture_file(path, :compute)
-        @logger.debug "[Backends] [DummyBackend] Reloading compute fixtures from #{path.inspect}"
-        compute = File.readable?(path) ? read_from_json(path).resources : Occi::Core::Resources.new
-        save_compute_fixtures(compute)
-      end
+  unless #{fixture}
+    path = path_for_fixture_file(path, :#{fixture})
+    @logger.debug "[Backends] [DummyBackend] Reloading #{fixture} fixtures from \"" + path.to_s + "\""
+    #{fixture} = File.readable?(path) ? read_from_json(path).resources : Occi::Core::Resources.new
+    save_#{fixture}_fixtures(#{fixture})
+  end
 
-      compute
+  #{fixture}
+end
+private :read_#{fixture}_fixtures
+
+def save_#{fixture}_fixtures(#{fixture})
+  Rails.env.test? ? @#{fixture} = #{fixture} : @dalli_cache.set('dummy_#{fixture}', #{fixture})
+end
+private :save_#{fixture}_fixtures
+
+def drop_#{fixture}_fixtures(lite = true)
+  if lite
+    save_#{fixture}_fixtures(Occi::Core::Resources.new)
+  else
+    Rails.env.test? ? @#{fixture} = nil : @dalli_cache.delete('dummy_#{fixture}')
+  end
+end
+private :drop_#{fixture}_fixtures
+|
     end
 
-    def save_compute_fixtures(compute)
-      Rails.env.test? ? @compute = compute : @dalli_cache.set('dummy_compute', compute)
-    end
+    FIXTURES_TPL.each do |fixture_tpl|
+      class_eval %Q|
+def read_#{fixture_tpl}_fixtures(path = '')
+  #{fixture_tpl} = Rails.env.test? ? @#{fixture_tpl} : @dalli_cache.get('dummy_#{fixture_tpl}')
 
-    def drop_compute_fixtures(lite = true)
-      if lite
-        save_compute_fixtures(Occi::Core::Resources.new)
-      else
-        Rails.env.test? ? @compute = nil : @dalli_cache.delete('dummy_compute')
-      end
-    end
+  unless #{fixture_tpl}
+    path = path_for_fixture_file(path, :#{fixture_tpl})
+    @logger.debug "[Backends] [DummyBackend] Reloading #{fixture_tpl} fixtures from \"" + path.to_s + "\""
+    #{fixture_tpl} = File.readable?(path) ? read_from_json(path).mixins : Occi::Core::Mixins.new
+    save_#{fixture_tpl}_fixtures(#{fixture_tpl})
+  end
 
-    def read_network_fixtures(path = '')
-      network = Rails.env.test? ? @network : @dalli_cache.get('dummy_network')
+  #{fixture_tpl}
+end
+private :read_#{fixture_tpl}_fixtures
 
-      unless network
-        path = path_for_fixture_file(path, :network)
-        @logger.debug "[Backends] [DummyBackend] Reloading network fixtures from #{path.inspect}"
-        network = File.readable?(path) ? read_from_json(path).resources : Occi::Core::Resources.new
-        save_network_fixtures(network)
-      end
-
-      network
-    end
-
-    def save_network_fixtures(network)
-      Rails.env.test? ? @network = network : @dalli_cache.set('dummy_network', network)
-    end
-
-    def drop_network_fixtures(lite = true)
-      if lite
-        save_network_fixtures(Occi::Core::Resources.new)
-      else
-        Rails.env.test? ? @network = nil : @dalli_cache.delete('dummy_network')
-      end
-    end
-
-    def read_storage_fixtures(path = '')
-      storage = Rails.env.test? ? @storage : @dalli_cache.get('dummy_storage')
-
-      unless storage
-        path = path_for_fixture_file(path, :storage)
-        @logger.debug "[Backends] [DummyBackend] Reloading storage fixtures from #{path.inspect}"
-        storage = File.readable?(path) ? read_from_json(path).resources : Occi::Core::Resources.new
-        save_storage_fixtures(storage)
-      end
-
-      storage
-    end
-
-    def save_storage_fixtures(storage)
-      Rails.env.test? ? @storage = storage : @dalli_cache.set('dummy_storage', storage)
-    end
-
-    def drop_storage_fixtures(lite = true)
-      if lite
-        save_storage_fixtures(Occi::Core::Resources.new)
-      else
-        Rails.env.test? ? @storage = nil : @dalli_cache.delete('dummy_storage')
-      end
-    end
-
-    def read_os_tpl_fixtures(path = '')
-      os_tpl = Rails.env.test? ? @os_tpl : @dalli_cache.get('dummy_os_tpl')
-
-      unless os_tpl
-        path = path_for_fixture_file(path, :os_tpl)
-        @logger.debug "[Backends] [DummyBackend] Reloading os_tpl fixtures from #{path.inspect}"
-        os_tpl = File.readable?(path) ? read_from_json(path).mixins : Occi::Core::Mixins.new
-        save_os_tpl_fixtures(os_tpl)
-      end
-
-      os_tpl
-    end
-
-    def save_os_tpl_fixtures(os_tpl)
-      Rails.env.test? ? @os_tpl = os_tpl : @dalli_cache.set('dummy_os_tpl', os_tpl)
-    end
-
-    def read_resource_tpl_fixtures(path = '')
-      resource_tpl = Rails.env.test? ? @resource_tpl : @dalli_cache.get('dummy_resource_tpl')
-
-      unless resource_tpl
-        path = path_for_fixture_file(path, :resource_tpl)
-        @logger.debug "[Backends] [DummyBackend] Reloading resource_tpl fixtures from #{path.inspect}"
-        resource_tpl = File.readable?(path) ? read_from_json(path).mixins : Occi::Core::Mixins.new
-        save_resource_tpl_fixtures(resource_tpl)
-      end
-
-      resource_tpl
-    end
-
-    def save_resource_tpl_fixtures(resource_tpl)
-      Rails.env.test? ? @resource_tpl = resource_tpl : @dalli_cache.set('dummy_resource_tpl', resource_tpl)
+def save_#{fixture_tpl}_fixtures(#{fixture_tpl})
+  Rails.env.test? ? @#{fixture_tpl} = #{fixture_tpl} : @dalli_cache.set('dummy_#{fixture_tpl}', #{fixture_tpl})
+end
+private :save_#{fixture_tpl}_fixtures
+|
     end
 
     def path_for_fixture_file(path, fixture_type)
@@ -135,7 +82,7 @@ module Backends
       fail Backends::Errors::ResourceRetrievalError, "Unable to read fixtures " \
            "from an unspecified directory!" if path.blank?
       fail Backends::Errors::ResourceRetrievalError, "Unable to read fixtures " \
-           "for #{fixture_type.to_s.inspect}!" unless FIXTURES.include? fixture_type
+           "for #{fixture_type.to_s.inspect}!" unless (FIXTURES + FIXTURES_TPL).include? fixture_type
 
       File.join(path, "#{fixture_type.to_s}.json")
     end
@@ -145,19 +92,6 @@ module Backends
 
     # hide internal stuff
     private :read_fixtures
-    private :read_storage_fixtures
-    private :read_network_fixtures
-    private :read_compute_fixtures
-    private :read_os_tpl_fixtures
-    private :read_resource_tpl_fixtures
-    private :save_storage_fixtures
-    private :save_network_fixtures
-    private :save_compute_fixtures
-    private :save_os_tpl_fixtures
-    private :save_resource_tpl_fixtures
-    private :drop_storage_fixtures
-    private :drop_network_fixtures
-    private :drop_compute_fixtures
     private :read_from_json
     private :path_for_fixture_file
 
