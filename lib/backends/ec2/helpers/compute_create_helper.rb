b9c853f69790f0c01bbdee48ee9894ddc1945d77
Boris Parak <parak@cesnet.cz>
2014-06-26 14:21:48 +0200

    Added support for resource_tpl in EC2 backend

--- lib/backends/ec2/helpers/compute_create_helper.rb
+++ lib/backends/ec2/helpers/compute_create_helper.rb
@@ -9,14 +9,18 @@ module Backends
           os_tpl_mixins = compute.mixins.get_related_to(Occi::Infrastructure::OsTpl.mixin.type_identifier)
           os_tpl = os_tpl_mixins.first
 
-          @logger.debug "[Backends] [Ec2Backend] Deploying with OS template: #{os_tpl.term}"
+          resource_tpl_mixins = compute.mixins.get_related_to(Occi::Infrastructure::ResourceTpl.mixin.type_identifier)
+          resource_tpl = resource_tpl_mixins.first
+
+          @logger.debug "[Backends] [Ec2Backend] Deploying with template #{os_tpl.term.inspect} in size #{resource_tpl.inspect}"
           os_tpl = os_tpl_list_term_to_image_id(os_tpl.term)
+          resource_tpl = resource_tpl_list_term_to_itype(resource_tpl ? resource_tpl.term : 't1_micro')
           serialized_mixins = compute.mixins.to_a.map { |m| m.type_identifier }.join(' ')
 
           Backends::Ec2::Helpers::AwsConnectHelper.rescue_aws_service(@logger) do
             ec2_response = @ec2_client.run_instances(
               image_id: os_tpl,
-              instance_type: 't1.micro',
+              instance_type: resource_tpl,
               min_count: 1, max_count: 1,
               user_data: '',
               monitoring: {
