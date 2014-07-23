343103adfe09405182eec9ef4c88e18444af54ce
Boris Parak <parak@cesnet.cz>
2014-07-23 17:32:06 +0200

    Added basic error handling for EC2 backend

--- lib/backends/ec2/helpers/aws_connect_helper.rb
+++ lib/backends/ec2/helpers/aws_connect_helper.rb
@@ -6,9 +6,34 @@ module Backends
         def self.rescue_aws_service(logger)
           begin
             yield
+          rescue ::Aws::EC2::Errors::ServiceError => e
+            handle_service_error(e, logger)
           rescue ::Seahorse::Client::Http::Error => e
-            logger.error "[Backends] [Ec2Backend] #{e.message}"
+            logger.error "[Backends] [Ec2Backend] HTTP Error: #{e.message}"
             fail Backends::Errors::ServiceUnavailableError, e.message
+          end
+        end
+
+        def self.handle_service_error(error, logger)
+          error_code = error.to_s.split('::').last
+          message = "#{error_code}: #{error.message}"
+
+          case error_code
+          when 'AuthFailure', 'Blocked'
+            fail Backends::Errors::AuthenticationError, message
+          when 'CannotDelete', 'DependencyViolation', 'IncorrectState', 'DiskImageSizeTooLarge', 'IncorrectInstanceState'
+            fail Backends::Errors::ResourceStateError, message
+          when /^(.+)Malformed$/, /^(.+)Format$/, /^(.+)ZoneMismatch$/, /^(.+)AlreadyExists$/, /^(.+)Duplicate$/
+            fail Backends::Errors::ResourceNotValidError, message
+          when /^(.+)InUse$/, /^(.+)Conflict$/, /^(.+)NotSupported$/
+            fail Backends::Errors::ResourceStateError, message
+          when /^Insufficient(.+)Capacity$/, /^(.+)LimitExceeded$/
+            fail Backends::Errors::ResourceCreationError, message
+          when /^(.+)NotFound$/
+            fail Backends::Errors::ResourceNotFoundError, message
+          else
+            # 'InternalError', 'Unavailable', ...
+            fail Backends::Errors::ResourceActionError, message
           end
         end
 
