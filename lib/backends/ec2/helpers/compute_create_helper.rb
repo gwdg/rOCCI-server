c33e0d6b5b3e7686520e8aa57dac109ab56cb272
Boris Parak <parak@cesnet.cz>
2014-08-11 20:54:25 +0200

    [WIP] Refactored helper for EC2 before inln links

--- lib/backends/ec2/helpers/compute_create_helper.rb
+++ lib/backends/ec2/helpers/compute_create_helper.rb
@@ -9,42 +9,25 @@ module Backends
         def compute_create_with_os_tpl(compute)
           @logger.debug "[Backends] [Ec2Backend] Deploying #{compute.inspect}"
 
-          os_tpl_mixins = compute.mixins.get_related_to(Occi::Infrastructure::OsTpl.mixin.type_identifier)
-          os_tpl = os_tpl_mixins.first
+          instance_opts = compute_create_instance_opts(compute)
+          instance_opts = compute_create_add_inline_ntwrkintfs(compute, instance_opts)
+          tags = compute_create_instance_tags(compute, instance_opts)
 
-          resource_tpl_mixins = compute.mixins.get_related_to(Occi::Infrastructure::ResourceTpl.mixin.type_identifier)
-          resource_tpl = resource_tpl_mixins.first
-
-          @logger.debug "[Backends] [Ec2Backend] Deploying with template #{os_tpl.term.inspect} in size #{resource_tpl.inspect}"
-          os_tpl = os_tpl_list_term_to_image_id(os_tpl.term)
-          resource_tpl = resource_tpl_list_term_to_itype(resource_tpl ? resource_tpl.term : 't1_micro')
-          serialized_mixins = compute.mixins.to_a.map { |m| m.type_identifier }.join(' ')
-
+          instance_id = nil
           Backends::Ec2::Helpers::AwsConnectHelper.rescue_aws_service(@logger) do
-            ec2_response = @ec2_client.run_instances(
-              image_id: os_tpl,
-              instance_type: resource_tpl,
-              min_count: 1, max_count: 1,
-              user_data: compute_create_user_data(compute),
-              monitoring: {
-                enabled: false,
-              },
-              placement: {
-                availability_zone: @options.aws_availability_zone,
-              }
-            )
-
+            ec2_response = @ec2_client.run_instances(instance_opts)
             instance_id = ec2_response.instances.first[:instance_id]
-            tags = []
-            tags << { key: 'Name', value: (compute.title || "rOCCI-server instance #{resource_tpl} + #{os_tpl}") }
-            tags << { key: 'ComputeMixins', value: serialized_mixins } if serialized_mixins.length < 255
+
             @ec2_client.create_tags(
               resources: [instance_id],
               tags: tags
             )
-
-            instance_id
           end
+
+          # TODO: attach inline storagelinks
+          # compute_create_add_inline_strglnks(compute, instance_id)
+
+          instance_id
         end
 
         private
@@ -61,6 +44,50 @@ module Backends
           end
 
           compute.attributes.org!.openstack!.compute!.user_data || ''
+        end
+
+        def compute_create_instance_opts(compute)
+          os_tpl_mixins = compute.mixins.get_related_to(Occi::Infrastructure::OsTpl.mixin.type_identifier)
+          os_tpl = os_tpl_mixins.first
+
+          resource_tpl_mixins = compute.mixins.get_related_to(Occi::Infrastructure::ResourceTpl.mixin.type_identifier)
+          resource_tpl = resource_tpl_mixins.first
+
+          @logger.debug "[Backends] [Ec2Backend] Deploying with template #{os_tpl.term.inspect} in size #{resource_tpl.inspect}"
+          os_tpl = os_tpl_list_term_to_image_id(os_tpl.term)
+          resource_tpl = resource_tpl_list_term_to_itype(resource_tpl ? resource_tpl.term : 't1_micro')
+
+          {
+            image_id: os_tpl,
+            instance_type: resource_tpl,
+            min_count: 1, max_count: 1,
+            user_data: compute_create_user_data(compute),
+            monitoring: {
+              enabled: false,
+            },
+            placement: {
+              availability_zone: @options.aws_availability_zone,
+            }
+          }
+        end
+
+        def compute_create_add_inline_ntwrkintfs(compute, instance_opts)
+          # TODO: impl
+          instance_opts
+        end
+
+        def compute_create_add_inline_strglnks(compute, instance_id)
+          # TODO: call compute_attach_storage(storagelink)
+        end
+
+        def compute_create_instance_tags(compute, instance_opts)
+          serialized_mixins = compute.mixins.to_a.map { |m| m.type_identifier }.join(' ')
+
+          tags = []
+          tags << { key: 'Name', value: (compute.title || "rOCCI-server instance #{instance_opts[:instance_type]} + #{instance_opts[:image_id]}") }
+          tags << { key: 'ComputeMixins', value: serialized_mixins } if serialized_mixins.length < 255
+
+          tags
         end
 
       end
