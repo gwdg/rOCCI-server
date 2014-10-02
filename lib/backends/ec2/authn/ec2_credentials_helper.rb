0c5bcbd58381448a6893bcea83e56bb7054d09c4
Boris Parak <parak@cesnet.cz>
2014-10-02 21:14:41 +0200

    [EC2] Added VO -> AWS account mapping
    Different VOs can now be mapped to different AWS accounts. This
    is performed by the EC2 backend internally.
    
    See 'etc/backends/ec2/files/mapfile.yaml' for details.

--- lib/backends/ec2/authn/ec2_credentials_helper.rb
+++ lib/backends/ec2/authn/ec2_credentials_helper.rb
@@ -23,7 +23,8 @@ module Backends::Ec2::Authn
         handle_voms(options, delegated_user, logger)
       else
         # unsupported authentication type
-        fail Backends::Errors::AuthenticationError, "Authentication strategy #{delegated_user.auth_.type.inspect} is not supported by the EC2 backend!"
+        fail Backends::Errors::AuthenticationError, "Authentication strategy " \
+             "#{delegated_user.auth_.type.inspect} is not supported by the EC2 backend!"
       end
     end
 
@@ -38,10 +39,15 @@ module Backends::Ec2::Authn
     # @param logger [Logger] instance of the logging facility
     # @return [::Aws::Credentials] credentials for the AWS EC2 client
     def self.handle_basic(options, delegated_user, logger)
-      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, username is missing!' if delegated_user.auth_.credentials_.username.blank?
-      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, password is missing!' if delegated_user.auth_.credentials_.password.blank?
+      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, ' \
+           'username is missing!' if delegated_user.auth_.credentials_.username.blank?
+      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, ' \
+           'password is missing!' if delegated_user.auth_.credentials_.password.blank?
 
-      ::Aws::Credentials.new(delegated_user.auth_.credentials_.username, delegated_user.auth_.credentials_.password)
+      ::Aws::Credentials.new(
+        delegated_user.auth_.credentials_.username,
+        delegated_user.auth_.credentials_.password
+      )
     end
 
     # Converts given X.509 credentials to credentials supported
@@ -53,8 +59,10 @@ module Backends::Ec2::Authn
     # @param logger [Logger] instance of the logging facility
     # @return [::Aws::Credentials] credentials for the AWS EC2 client
     def self.handle_x509(options, delegated_user, logger)
-      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, global access_key_id is missing!' if options.access_key_id.blank?
-      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, global secret_access_key is missing!' if options.secret_access_key.blank?
+      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, ' \
+           'global access_key_id is missing!' if options.access_key_id.blank?
+      fail Backends::Errors::AuthenticationError, 'User could not be authenticated, ' \
+           'global secret_access_key is missing!' if options.secret_access_key.blank?
 
       ::Aws::Credentials.new(options.access_key_id, options.secret_access_key)
     end
@@ -70,8 +78,38 @@ module Backends::Ec2::Authn
     # @param logger [Logger] instance of the logging facility
     # @return [::Aws::Credentials] credentials for the AWS EC2 client
     def self.handle_voms(options, delegated_user, logger)
-      # TODO: implement credential switching based on VOs
-      handle_x509(options, delegated_user, logger)
+      # select the first VO
+      # TODO: What to do with multiple VOs?
+      first_vo = (delegated_user.auth_.credentials_.client_cert_voms_attrs || []).first
+      fail Backends::Errors::AuthenticationError, 'Malformed VOMS credentials, ' \
+           'required extension attributes are missing!' unless first_vo && !first_vo.vo.blank?
+
+      logger.debug "[Backends] [Ec2Backend] Searching for VO -> AWS " \
+                   "account mapping for #{first_vo.vo.inspect}"
+      if !options.vo_aws_mapfile.blank?
+        # we have a mapfile with VO -> AWS account entries
+        vo_aws_mapfile = AuthenticationStrategies::Helpers::YamlHelper.read_yaml(options.vo_aws_mapfile)
+
+        # look-up the VO name
+        if vo_aws_mapfile.include?(first_vo.vo)
+          # mapping was provided, use it
+          logger.debug "[Backends] [Ec2Backend] Found mapping for " \
+                       "#{first_vo.vo.inspect} in #{options.vo_aws_mapfile.inspect}"
+          ::Aws::Credentials.new(
+            vo_aws_mapfile[first_vo.vo].access_key_id,
+            vo_aws_mapfile[first_vo.vo].secret_access_key
+          )
+        else
+          # no mapping was provided, use the global default
+          logger.debug "[Backends] [Ec2Backend] No mapping for " \
+                       "#{first_vo.vo.inspect} in #{options.vo_aws_mapfile.inspect}"
+          handle_x509(options, delegated_user, logger)
+        end
+      else
+        # no mapfile was provided
+        logger.debug "[Backends] [Ec2Backend] No VO -> AWS account mapfile was provided!"
+        handle_x509(options, delegated_user, logger)
+      end
     end
 
   end
