cecb937617e122b9e7aad63c6f3b6a96447778c3
Boris Parak <parak@cesnet.cz>
2014-10-11 21:02:53 +0200

    Added options for OpenSSL peer verification

--- config/initializers/force_ssl.rb
+++ config/initializers/force_ssl.rb
@@ -1,2 +1,5 @@
 # Force SSL according to settings in Rails.application.config.rocci_server_etc_dir/ENV.yml
 Rails.application.config.force_ssl = (ROCCI_SERVER_CONFIG.common.force_ssl.to_s == 'true')
+
+# Disable mandatory peer verification if SSL_CERT_* vars are not available
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if ENV['SSL_CERT_FILE'].blank? && ENV['SSL_CERT_DIR'].blank?
