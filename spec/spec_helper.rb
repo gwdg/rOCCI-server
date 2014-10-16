883bb5e8898ff2c07727fb8c7315cd482efda572
Zdenek Sustr <sustr4@cesnet.cz>
2014-10-16 12:41:55 +0200

    Interpret spec location as type
    Automatically infer an example group's spec type from the file location

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,4 +40,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  #automatically infer an example group's spec type from the file location.
+  config.infer_spec_type_from_file_location!
 end
