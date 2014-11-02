8377b07005a42f48fc07f0e0a5c71539f0563915
Boris Parak <parak@cesnet.cz>
2014-11-02 21:12:04 +0100

    Added skeleton impl of the new autocreate hook

---
+++ lib/hooks/oneuser_autocreate_hook.rb
@@ -0,0 +1,12 @@
+module Hooks
+  class OneuserAutocreateHook
+    def initialize(app, options)
+      @app = app
+      @options = options
+    end
+
+    def call(env)
+      @app.call(env)
+    end
+  end
+end
