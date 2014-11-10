f1499e2cd0ebfafb5e17cf942f890a27b0b7d498
Zdenek Sustr <sustr4@cesnet.cz>
2014-11-10 16:55:56 +0100

    Additional spec in progress, not working though

--- spec/lib/backends/ec2_backend_spec.rb
+++ spec/lib/backends/ec2_backend_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+#require 'ruby-debug'
 
 describe Backends::Ec2Backend do
   let(:dalli) { Dalli::Client.new }
@@ -16,9 +17,17 @@ describe Backends::Ec2Backend do
 
   context 'compute' do
     describe 'compute_list_ids' do
-      it 'runs' do
+      it 'runs with empty list' do
         expect(ec2_backend_instance.compute_list_ids).to eq([])
       end
+
+      it 'Receives compute instance list correctly' do
+        ec2_dummy_client.stub_responses("DescribeInstanceStatus", instance_status_set:[{instance_id:'ID',availability_zone:"zone"}])
+
+#        debugger
+        expect(ec2_backend_instance.compute_list_ids).to eq([])
+      end
+
     end
   end
 
