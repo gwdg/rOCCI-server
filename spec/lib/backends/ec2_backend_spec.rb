91e973287310f453173792c780a90c194227cfab
Zdenek Sustr <git@sustr.net>
2014-11-11 16:15:14 +0100

    Next test, one more styb yaml. Breaks for now

--- spec/lib/backends/ec2_backend_spec.rb
+++ spec/lib/backends/ec2_backend_spec.rb
@@ -6,6 +6,7 @@ describe Backends::Ec2Backend do
   let(:aws_creds) { ::Aws::Credentials.new('a', 'b') }
   let(:ec2_dummy_client) { ::Aws::EC2::Client.new(credentials: aws_creds, stub_responses: true) }
   let(:instance_statuses_stub) { YAML.load_file("spec/lib/backends/ec2_stubs/instance_statuses_stub.yaml") }
+  let(:reservations_stub) { YAML.load_file("spec/lib/backends/ec2_stubs/reservations_stub.yaml") }
   let(:ec2_backend_instance) do
     instance = Backends::Ec2Backend.new nil, nil, nil, nil, dalli
     instance.instance_variable_set(:@ec2_client, ec2_dummy_client)
@@ -24,11 +25,19 @@ describe Backends::Ec2Backend do
 
       it 'Receives compute instance list correctly' do
         ec2_dummy_client.stub_responses(:describe_instance_status, instance_statuses:instance_statuses_stub)
-
-
         expect(ec2_backend_instance.compute_list_ids).to eq(["ID", "ID2"])
       end
+    end
 
+    describe '.compute_list' do
+      it 'runs with empty list' do
+        expect(ec2_backend_instance.compute_list).to eq([])
+      end
+
+      it 'Receives compute instance list correctly' do
+        ec2_dummy_client.stub_responses(:describe_instance_status, reservations:reservations_stub)
+        expect(ec2_backend_instance.compute_list).to eq(["ID", "ID2"])
+      end
     end
   end
 
