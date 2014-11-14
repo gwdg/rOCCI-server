40be2094318325e61d78df0f1ea15bd424e9c921
Zdenek Sustr <git@sustr.net>
2014-11-14 08:20:34 +0100

    Candidate to fix Issue #84
    - Do not fail on nil sets
    - + Test

--- lib/backends/ec2/helpers/compute_parse_helper.rb
+++ lib/backends/ec2/helpers/compute_parse_helper.rb
@@ -91,16 +91,17 @@ module Backends
             link.id = id
             link.state = (compute.state == 'active') ? 'active' : 'inactive'
 
-            target = storage_get(blk[:ebs][:volume_id])
+            if (target = storage_get(blk[:ebs][:volume_id])) 
 
-            link.target = target
-            link.rel = target.kind
-            link.title = target.title if target.title
-            link.source = compute
+              link.target = target
+              link.rel = target.kind
+              link.title = target.title if target.title
+              link.source = compute
 
-            link.deviceid = blk[:device_name] if blk[:device_name]
+              link.deviceid = blk[:device_name] if blk[:device_name]
 
-            result_storage_links << link
+              result_storage_links << link
+            end
           end
 
           result_storage_links
@@ -163,10 +164,12 @@ module Backends
             link.address = intf[:private_ip_address]
           end
 
-          link.target = target
-          link.rel = target.kind
-          link.title = target.title if target.title
-          link.source = compute
+          if (target)
+            link.target = target
+            link.rel = target.kind
+            link.title = target.title if target.title
+            link.source = compute
+          end
 
           link
         end
