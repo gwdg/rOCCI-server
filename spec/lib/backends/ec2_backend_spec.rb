e94bb932e3783683ba73b24803a25c04e58ede3d
Zdenek Sustr <git@sustr.net>
2014-11-18 16:31:55 +0100

    Attempt at .compute_create, failing

--- spec/lib/backends/ec2_backend_spec.rb
+++ spec/lib/backends/ec2_backend_spec.rb
@@ -71,6 +71,21 @@ describe Backends::Ec2Backend do
         expect(returned).to eq expected
       end
     end
+
+    describe '.compute_create' do
+      it 'reports correctly on missing os_tpl mixin' do
+        compute = Occi::Infrastructure::Compute.new
+        expect{ec2_backend_instance.compute_create(compute)}.to raise_exception(Backends::Errors::ResourceNotValidError)
+      end
+
+      it 'creates compute resource correctyly' do
+        ostemplate = Occi::Core::Mixin.new("http://occi.localhost/occi/infrastructure/os_tpl#", "ami-6e7bd919")
+        ostemplate.depends=["http://schemas.ogf.org/occi/infrastructure#os_tpl"]
+        compute = Occi::Infrastructure::Compute.new
+        compute.mixins << ostemplate
+        expect(ec2_backend_instance.compute_create(compute)).to eq []
+      end
+    end
   end
 
 end
