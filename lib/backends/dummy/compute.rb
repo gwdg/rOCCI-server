3c475b918c8fa0e0fdda6323d53301b3574b2828
Boris Parak <parak@cesnet.cz>
2015-03-09 20:05:15 +0100

    [Docs] Fixed a typo in docu comments

--- lib/backends/dummy/compute.rb
+++ lib/backends/dummy/compute.rb
@@ -345,7 +345,7 @@ module Backends
         storagelink.id
       end
 
-      # Dettaches a network from an existing compute instance, the compute instance in question
+      # Detaches a network from an existing compute instance, the compute instance in question
       # must be identifiable using the networkinterface ID passed as an argument.
       # If the requested link instance cannot be detached, an error describing the
       # problem must be raised, @see Backends::Errors.
@@ -387,7 +387,7 @@ module Backends
         true
       end
 
-      # Dettaches a storage from an existing compute instance, the compute instance in question
+      # Detaches a storage from an existing compute instance, the compute instance in question
       # must be identifiable using the storagelink ID passed as an argument.
       # If the requested link instance cannot be detached, an error describing the
       # problem must be raised, @see Backends::Errors.
