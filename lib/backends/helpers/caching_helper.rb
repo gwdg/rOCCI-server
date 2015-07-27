0fc958de582ccf00c8856aea39e5d424cb82464e
Boris Parak <parak@cesnet.cz>
2015-07-27 13:50:09 +0200

    [Ref] Cleaned up helpers + docs

--- lib/backends/helpers/caching_helper.rb
+++ lib/backends/helpers/caching_helper.rb
@@ -1,7 +1,13 @@
 module Backends
   module Helpers
+    # Helps with cache handling with Dalli.
     module CachingHelper
-
+      # Saves `data` under `key` in the given caching instance.
+      #
+      # @param dalli_instance [::Dalli::Client] caching instance
+      # @param key [String] key value
+      # @param data [Object] data to store
+      # @return [Boolean] success or failure
       def self.save(dalli_instance, key, data)
         return if dalli_instance.blank? || key.blank?
 
@@ -14,6 +20,11 @@ module Backends
         end
       end
 
+      # Loads `data` under `key` from the given caching instance.
+      #
+      # @param dalli_instance [::Dalli::Client] caching instance
+      # @param key [String] key value
+      # @return [Object,NilClass] data or nothing
       def self.load(dalli_instance, key)
         return if dalli_instance.blank? || key.blank?
 
@@ -24,6 +35,11 @@ module Backends
         end
       end
 
+      # Drops `data` under `key` in the given caching instance.
+      #
+      # @param dalli_instance [::Dalli::Client] caching instance
+      # @param key [String] key value
+      # @return [Boolean] success or failure
       def self.drop(dalli_instance, key)
         return if dalli_instance.blank? || key.blank?
 
@@ -35,7 +51,6 @@ module Backends
           false
         end
       end
-
     end
   end
 end
