85c50a70f23d7faaecc6d8e221ab458e6c9e0dd3
Boris Parak <parak@cesnet.cz>
2017-02-10 15:07:00 +0100

    [ONE] Fixed wonky handling of res_tpl attributes
    In previous versions, users were allowed to override compute attrs
    used for sizing the instance. This is no longer possible since only
    server-side values of the provided template are passed on to ONe.

--- lib/backends/opennebula/helpers/compute_parse_helper.rb
+++ lib/backends/opennebula/helpers/compute_parse_helper.rb
@@ -86,8 +86,7 @@ module Backends
           compute_attrs['occi.compute.memory'] = (backend_compute['TEMPLATE/MEMORY'].to_f / 1024)
           compute_attrs['occi.compute.speed'] = (backend_compute['TEMPLATE/CPU'] || 1).to_f / compute_attrs['occi.compute.cores']
 
-          compute_attrs['occi.compute.architecture'] = 'x64' if backend_compute['TEMPLATE/OS/ARCH'] == 'x86_64'
-          compute_attrs['occi.compute.architecture'] = 'x86' if backend_compute['TEMPLATE/OS/ARCH'] == 'i686'
+          compute_attrs['occi.compute.architecture'] = (backend_compute['TEMPLATE/OS/ARCH'] == 'i686') ? 'x86' : 'x64'
 
           compute_attrs
         end
