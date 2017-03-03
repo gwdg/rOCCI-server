0dba611014767bedefe4a21a3f22181125427967
František Dvořák <valtri@civ.zcu.cz>
2017-03-03 16:19:15 +0100

    [NOW] Disable unit-tests, keep only the skeleton

--- spec/lib/backends/now/network_spec.rb
+++ spec/lib/backends/now/network_spec.rb
@@ -2,18 +2,24 @@ require 'spec_helper'
 
 describe Backends::Now::Network do
   let(:dalli) { Dalli::Client.new }
-  let(:dummy_w_opts) do
-    opts = Hashie::Mash.new
-    opts.fixtures_dir = Rails.application.config.rocci_server_etc_dir.join('backends', 'now', 'fixtures')
-    Backends::Now::Network.new nil, opts, nil, nil, dalli
-  end
-
   before(:each) { dalli.flush }
   after(:all) { Dalli::Client.new.flush }
 
+  let(:now_backend_delegated_user) do
+    user = Hashie::Mash.new
+    user.identity = 'now_test_user'
+    user
+  end
+
+  let(:now_backend_instance) do
+    options = YAML.load(ERB.new(File.read("#{Rails.root}/etc/backends/now/test.yml")).result)
+    instance = Backends::Now::Network.new(now_backend_delegated_user, options, nil, nil, dalli)
+    instance
+  end
+
   describe '#new' do
-    it 'fails to instantiate without a fixtures_dir' do
-      expect { Backends::Now::Network.new nil, nil, nil, nil, dalli }.to raise_error
+    it 'ok' do
+      expect(now_backend_instance).to be_instance_of(Backends::Now::Network)
     end
   end
 end
