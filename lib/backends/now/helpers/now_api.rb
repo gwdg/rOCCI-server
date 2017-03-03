d1f8e66656e0e836b22645c19e26ee39381dd2ad
František Dvořák <valtri@civ.zcu.cz>
2017-03-03 18:09:50 +0100

    [NOW] Use constants for HTTP codes

--- lib/backends/now/helpers/now_api.rb
+++ lib/backends/now/helpers/now_api.rb
@@ -7,13 +7,16 @@ module Backends
       class NowApi
         include HTTParty
 
+        HTTP_OK = '200'.freeze
+        HTTP_CREATED = '201'.freeze
+
         def initialize(user, options)
           @http_options = { base_uri: options.endpoint, query: { user: user } }
           @options = options
         end
 
         def get(id)
-          result = check('200') do
+          result = check(HTTP_OK) do
             self.class.get("/network/#{id}", @http_options)
           end
 
@@ -21,7 +24,7 @@ module Backends
         end
 
         def list
-          result = check('200') do
+          result = check(HTTP_OK) do
             result = self.class.get('/network', @http_options)
           end
 
@@ -31,7 +34,7 @@ module Backends
         def create(network)
           network.delete 'id'
           @http_options[:body] = network.to_json
-          result = check('201') do
+          result = check(HTTP_CREATED) do
             result = self.class.post('/network', @http_options)
           end
 
@@ -39,7 +42,7 @@ module Backends
         end
 
         def delete(id)
-          result = check('200') do
+          result = check(HTTP_OK) do
             result = self.class.delete("/network/#{id}", @http_options)
           end
 
@@ -49,7 +52,7 @@ module Backends
         def update(id, network)
           network.delete 'id'
           @http_options[:body] = network.to_json
-          result = check('200') do
+          result = check(HTTP_OK) do
             result = self.class.put("/network/#{id}", @http_options)
           end
 
