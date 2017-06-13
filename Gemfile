e5010e1668bcf6a0a0d63d39e313cbd7210de31e
Boris Parak <parak@cesnet.cz>
2017-06-13 15:14:02 +0200

    Cleaned up unused Rails components and deps

--- Gemfile
+++ Gemfile
@@ -6,17 +6,13 @@ git_source(:github) do |repo_name|
 end
 
 gem 'rails', '~> 5.1.1'
-gem 'warden', '~> 1.2.7'
-gem 'mongo'
+gem 'puma', '~> 3.7'
+gem 'rack-cors', '~> 0.4'
+gem 'logstasher', '~> 1.2'
 
-gem 'rack-cors'
-gem 'logstasher'
-gem 'bson'
-
-gem 'occi-core', '= 5.0.0.beta.1', require: 'occi/infrastructure-ext' # '~> 5.0.0'
+gem 'occi-core', '= 5.0.0.beta.2', require: 'occi/infrastructure-ext' # '~> 5.0.0'
 
 group :development, :test do
-  gem 'puma', '~> 3.7'
   gem 'byebug'
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'spring'
@@ -24,10 +20,8 @@ group :development, :test do
 end
 
 # Include external bundles
-%w[authentication_strategies backends].each do |subdir|
-  path = File.join(File.dirname(__FILE__), 'lib', subdir, 'bundles')
-  next unless File.directory?(path)
-
+path = File.join(File.dirname(__FILE__), 'lib', 'backends', 'bundles')
+if File.directory?(path)
   Dir.glob(File.join(path, "Gemfile.*")) do |gemfile|
       eval(IO.read(gemfile), binding)
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,9 +50,9 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rOCCI-server_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  # config.active_job.queue_adapter      = :sidekiq
+  # config.active_job.queue_name_prefix  = "rOCCI-server_#{Rails.env}"
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  # config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
@@ -79,5 +79,5 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
 end
