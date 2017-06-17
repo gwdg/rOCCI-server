d0a7149e879c25d1ebd378264853fa97bd34ac76
Boris Parak <parak@cesnet.cz>
2017-06-17 15:05:11 +0200

    Reworked controller structure
    - `EntityController` for stuff routed from `/mixin/*` and `/`
    - `MixinController` only for creating and deleting custom mixins
    - Fixed token lookup (headers are always in caps)
    - Switched to strict media type validation (fallback only for `*/*`)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ require 'application_responder'
 require 'renderable_error'
 
 class ApplicationController < ActionController::API
-  ANY_FORMAT = '*/*'.freeze
-  NO_FORMAT = ''.freeze
-  WRONG_FORMATS = [ANY_FORMAT, NO_FORMAT].freeze
-  DEFAULT_FORMAT_SYM = :text
+  URI_FORMATS = %i[uri_list]
+  FULL_FORMATS = %i[json text headers]
+  ALL_FORMATS = [URI_FORMATS, FULL_FORMATS].flatten.freeze
+  UBIQUITOUS_FORMATS = %w[*/*].freeze
+  DEFAULT_FORMAT_SYM = FULL_FORMATS.first
+
+  TOKEN_HEADER_KEY = 'HTTP_X_AUTH_TOKEN'.freeze
   REDIRECT_HEADER_KEY = 'WWW-Authenticate'.freeze
+  MODEL_FLAVORS = %w[core infrastructure infrastructure_ext].freeze
 
   # Force SSL, we live in the 21st century after all
   force_ssl
@@ -14,11 +18,11 @@ class ApplicationController < ActionController::API
   # Register supported MIME formats
   # @see 'config/initializers/mime_types.rb' for details
   self.responder = ApplicationResponder
-  respond_to :uri_list, only: [:locations]
-  respond_to :json, :text, :headers
+  respond_to(*URI_FORMATS, only: %i[locations])
+  respond_to(*FULL_FORMATS)
 
   # Run pre-action checks
-  before_action :default_format!
+  before_action :validate_format!
   before_action :authorize_user!
   before_action :validate_url_param
 
@@ -45,17 +49,32 @@ class ApplicationController < ActionController::API
     @current_user || 'unauthorized'
   end
 
+  def server_model
+    @model || bootstrap_server_model!
+  end
+
+  def bootstrap_server_model!
+    @model = Occi::InfrastructureExt::Model.new
+    MODEL_FLAVORS.each { |flv| @model.send "load_#{flv}!" }
+    @model.valid!
+
+    @model
+  end
+
   private
 
-  # Checks request format and sets the default 'text/plain' if necessary.
-  def default_format!
-    return unless WRONG_FORMATS.include?(request.format.to_s)
-    logger.debug "Request format in #{WRONG_FORMATS.inspect}, forcing #{DEFAULT_FORMAT_SYM} for compatibility"
-    request.format = DEFAULT_FORMAT_SYM
+  def validate_format!
+    if UBIQUITOUS_FORMATS.include?(request.format.to_s)
+      logger.debug "Changing ubiquitous format #{request.format.to_s.inspect} to #{DEFAULT_FORMAT_SYM.inspect}"
+      request.format = DEFAULT_FORMAT_SYM
+    end
+
+    return if ALL_FORMATS.include?(request.format.symbol)
+    render_error 406, 'Requested media format is not acceptable'
   end
 
   def authorize_user!
-    if request.env['HTTP_X_Auth_Token'].blank?
+    if request.env[TOKEN_HEADER_KEY].blank?
       auth_redirect_header!
       render_error 401, 'Not Authorized'
     else
