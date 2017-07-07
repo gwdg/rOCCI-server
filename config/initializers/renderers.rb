ad2b6b3f040da3d07a9adf09d44542593d196f69
Boris Parak <parak@cesnet.cz>
2017-07-07 11:08:48 +0200

    Support for locations in `Set`s

--- config/initializers/renderers.rb
+++ config/initializers/renderers.rb
@@ -1,6 +1,7 @@
 # Render OCCI locations into HTTP body for 'text/uri-list'
 ActionController::Renderers.add :uri_list do |obj, _options|
   self.content_type = Mime::Type.lookup_by_extension(:uri_list)
+  obj = obj.to_a if obj.respond_to?(:to_a)
   obj.respond_to?(:join) ? obj.join("\n") : obj.to_s
 end
 
