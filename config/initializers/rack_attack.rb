77d2b0911342585ad6eadd9f053f758b9fccd5f8
Boris Parak <parak@cesnet.cz>
2017-07-07 11:07:31 +0200

    Reasonable default config for Rack::Attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,10 @@
 # Rack attack protection
 module Rack
   class Attack
-    throttle('req/ip', limit: 300, period: 1.minute, &:ip)
+    throttle('req/ip', limit: 120, period: 1.minute, &:ip)
 
     blocklist('auth-less annoyances') do |req|
-      Allow2Ban.filter(req.ip, maxretry: 10, findtime: 1.minute, bantime: 1.hour) do
+      Allow2Ban.filter(req.ip, maxretry: 10, findtime: 1.minute, bantime: 24.hours) do
         req.env[Tokenator::TOKEN_HEADER_KEY].blank?
       end
     end
