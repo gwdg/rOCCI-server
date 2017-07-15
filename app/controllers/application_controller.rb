1b87c68a47979b36e2b52679730be97a583739b6
Boris Parak <parak@cesnet.cz>
2017-07-15 19:02:48 +0200

    SSL already forced in env files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,6 @@ class ApplicationController < ActionController::API
   include BackendAccessible
   include ModelAccessible
 
-  # Just in case
-  force_ssl
-
   # More convenient access to logging
   delegate :debug?, prefix: true, to: :logger
 end
